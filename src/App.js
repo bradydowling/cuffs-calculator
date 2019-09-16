0e7fb6224e056eadb35410fc9650ee1ca5feda66
Brady Dowling <brady@amazon.com>
2019-09-16 10:27:50 -0400

    First working prototype

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends React.Component {
     super();
     this.state = {
       value: 0,
-      cuffDate: '01-01-2000',
-      releaseDate: '01-01-2100'
+      cuffDate: null,
+      releaseDate: null
     };
   }
 
@@ -23,13 +23,33 @@ class App extends React.Component {
     this.setState({ value });
   }
 
-  getDateDiff() {
+  getCuffTimeDetails() {
+    if (!this.state.cuffDate || !this.state.releaseDate) {
+      return {};
+    }
     var dashReplacer = /-/gi;
-    const date1 = new Date(this.state.cuffDate.replace(dashReplacer, '/'));
-    const date2 = new Date(this.state.releaseDate.replace(dashReplacer, '/'));
-    const diffTime = Math.abs(date2 - date1);
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    return diffDays;
+    const cuffDate = new Date(this.state.cuffDate.replace(dashReplacer, '/'));
+    const todayDate = new Date();
+    const releaseDate = new Date(this.state.releaseDate.replace(dashReplacer, '/'));
+    const totalCuffTime = Math.abs(releaseDate - cuffDate);
+    const totalCuffDays = Math.ceil(totalCuffTime / (1000 * 60 * 60 * 24));
+    const servedCuffTime = Math.abs(todayDate - cuffDate);
+    const servedCuffDays = Math.ceil(servedCuffTime / (1000 * 60 * 60 * 24));
+    const remainingCuffTime = Math.abs(releaseDate - todayDate);
+    const remainingCuffDays = Math.ceil(remainingCuffTime / (1000 * 60 * 60 * 24));
+    return {
+      total: totalCuffDays,
+      served: servedCuffDays,
+      remaining: remainingCuffDays
+    };
+  }
+
+  getCuffValue() {
+    const cuffDetails = this.getCuffTimeDetails();
+    if (!cuffDetails.remaining || !cuffDetails.total || !this.state.value) {
+      return 0;
+    }
+    return cuffDetails.remaining / cuffDetails.total * this.state.value;
   }
 
   render() {
@@ -43,7 +63,7 @@ class App extends React.Component {
           Handcuff value (signing bonus, move cost, etc)
           <input type="number" name="cuff-value" min="0" step="1000" defaultValue="30" onChange={e => { this.valueChange(e.target.value); }}></input>
           If you left today, you would owe:
-          {this.getDateDiff()}
+          {this.getCuffValue()}
         </header>
       </div>
     );
